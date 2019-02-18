13348b9492dfc9a8453cda3f00b4ebeed0491d55
deadsidestar <sxycoup75@protonmail.com>
2019-02-18 12:26:00 -0800

    xmobar on 2 screens

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -351,11 +351,11 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 --XMOBAR
 main = do
             
-        xmproc1 <- spawnPipe "xmobar -x 1 $HOME/.xmobarrc" -- xmobar monitor 1
-        xmproc2 <- spawnPipe "xmobar -x 2 $HOME/.xmobarrc" -- xmobar monitor 2
+        xmproc0 <- spawnPipe "xmobar -x 1 $HOME/.xmobarrc" -- xmobar monitor 1
+        xmproc1 <- spawnPipe "xmobar -x 2 $HOME/.xmobarrc" -- xmobar monitor 2
         xmonad $ ewmh $ mydefaults {
         logHook =  dynamicLogWithPP $ def {
-        ppOutput = \x -> hPutStrLn xmproc1 x  >> hPutStrLn xmproc2 x
+        ppOutput = \x -> System.IO.hPutStrLn xmproc0 x  >> hPutStrLn xmproc1 x
         , ppTitle = xmobarColor myTitleColor "" . ( \ str -> "")
         , ppCurrent = xmobarColor myCurrentWSColor "" . wrap """"
         , ppVisible = xmobarColor myVisibleWSColor "" . wrap """"
